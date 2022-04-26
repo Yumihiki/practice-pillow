c2ae6846d9ca244c662dc5af6d67da904a01c11a
Yumihiki <nibu10.majik@gmail.com>
2022-04-26 22:20:30 +0900

    withを用いた内容に修正

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from PIL import Image
 
 
-image = Image.open('画像名')
-width, height = image.size
-print(width)
-print(height)
+# ファイルの縦横比を調べる
+with Image.open('shoutingChickenIconBlackLine2.jpg') as im:
+    width, height = im.size
+    print(width)
+    print(height)
