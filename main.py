9f04848515f97f165f4aeda10002e8935c15dad2
Yumihiki <nibu10.majik@gmail.com>
2022-04-27 22:55:49 +0900

    角度を変化させるコードの追加

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from PIL import Image
-
+import glob
+import os
 
 # ファイルの縦横比を調べる
 with Image.open('shoutingChickenIconBlackLine2.jpg') as im:
     width, height = im.size
     print(width)
     print(height)
+
+# 角度を変化させる
+with Image.open('shoutingChickenIconBlackLine2.jpg') as im2:
+    im2.rotate(45).show()
