12b6412903baef32fa1414a75935920acd32353b
Yumihiki <nibu10.majik@gmail.com>
2022-04-28 23:22:27 +0900

    画像を元にして生成するPGの記述

--- main.py
+++ main.py
@@ -11,3 +11,11 @@ with Image.open('shoutingChickenIconBlackLine2.jpg') as im:
 # 角度を変化させる
 with Image.open('shoutingChickenIconBlackLine2.jpg') as im2:
     im2.rotate(45).show()
+
+# 画像を元に生成する
+size = 128, 128
+for infile in glob.glob("shoutingChickenIconBlackLine2.jpg"):
+    file, ext = os.path.splitext(infile)
+    with Image.open(infile) as im:
+        im.thumbnail(size)
+        im.save(file + "resize.jpg", "JPEG")
